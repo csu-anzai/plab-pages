675ffd13e15cfa20debc47c7573d7aa86eb7a9b6
Xavier Loo <xavier@printinglab.my>
2019-09-02 17:28:50 +0800

    Initial commit for campaign "Malaysia Day Special Giveaway 2019"

---
+++ application/views/malaysia-day-special-giveaway-2019/choose-template.php
@@ -0,0 +1,189 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$email = '';
+$error = '';
+
+if ( ! empty($flashdata = $this->session->flashdata('free_name_card_20190709__error'))) {
+  $email = $flashdata['email'];
+  $error = $flashdata['error'];
+}
+
+$text = htmlentities("Hi, I have encountered an error '{$error}' when trying to sign up to the promotional campaign 'free-name-card-20190709' using email address '{$email}'.");
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="XK Printing (002952616-U)">
+  <meta name="robots" content="NOINDEX, NOFOLLOW">
+
+  <title>Free Name Card Promotion - by PrintingLab MY</title>
+
+  <link rel="shortcut icon" type="image/x-icon" href="https://static.printinglab.my/img/common/favicon-48x48.ico" sizes="48x48">
+
+  <link rel="stylesheet" href="https://static.printinglab.my/vendor/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Righteous:400|Merriweather+Sans:700|Roboto:400,500,700|Amaranth|Lato|Hind|Josefin+Sans:400,700">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous" >
+
+  <link rel="stylesheet" href="https://static.printinglab.my/css/plan-b/style.css">
+  <link rel="stylesheet" href="https://static.printinglab.my/css/campaign/free-name-card-20190406/landing.css">
+
+  <script src="https://static.printinglab.my/vendor/jquery/3.3.1/jquery.min.js"></script>
+  <!-- Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-120946405-2"></script>
+  <script>/*
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-120946405-2');
+  </script>
+  <!-- END Google Analytics -->
+  <!-- Facebook Pixel Code -->
+  <script>/*
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '633239817127685');
+    fbq('track', 'PageView');
+    fbq('track', 'ViewContent', {
+      content_category: 'campaign',
+      content_name: 'Free Name Card Promotion 20190406',
+    });
+
+    $(function() {
+      $('.px-OrderLead').on('click', function() {
+        fbq('track', 'Lead', {
+          content_category: 'order',
+          content_name: 'Proceed To Form',
+        })
+      })
+    })
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=633239817127685&ev=PageView&noscript=1"
+  /></noscript>
+  <!-- End Facebook Pixel Code -->
+  <!-- Hotjar Tracking Code -->
+  <script>/*
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1172598,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  </script>
+  <!-- END Hotjar Tracking Code -->
+
+  <style>
+    header.masthead {
+      padding-top: 2rem;
+      padding-bottom: 2rem;
+    }
+
+    header.masthead .masthead-content .display-4 {
+      font-size: 2rem;
+    }
+
+    header.masthead .masthead-content .masthead-heading {
+      font-size: 1.5rem
+    }
+
+    header.masthead .masthead-content .masthead-subheading {
+      font-size: 1.25rem
+    }
+
+    header.masthead .masthead-content .masthead-heading,
+    header.masthead .masthead-content .masthead-subheading {
+      line-height: normal;
+    }
+
+    h1 {
+      font-size: 1.75rem;
+    }
+
+    .btn-xl {
+      padding: 1rem 2.5rem;
+    }
+
+    @media (min-width: 576px) {
+      header.masthead {
+        padding-top: 7rem;
+        padding-bottom: 7rem;
+      }
+
+      header.masthead .masthead-content .display-4 {
+        font-size: 3.5rem;
+      }
+
+      header.masthead .masthead-content .masthead-heading {
+        font-size: 3rem
+      }
+
+      header.masthead .masthead-content .masthead-subheading {
+        font-size: 2rem
+      }
+
+      h1 {
+        font-size: 2.345rem;
+      }
+
+      .btn-xl {
+        padding: 1.5rem 3rem;
+      }
+    }
+
+    .masthead-notice {
+      position: absolute;
+      bottom: -5rem;
+      width: 100%;
+      left:0;
+    }
+  </style>
+</head>
+<body>
+  <div class="bg-gradient bg-white">
+    <nav class="navbar navbar-expand-lg shadow">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="https://static.printinglab.my/img/plan-b/PrintingLab-logo-v2.png">
+        </a>
+      </div>
+    </nav>
+  </div>
+
+  <!--- Section Masthead --->
+  <header class="masthead text-center text-white shadow-sm">
+    <!--- Masthead Content --->
+    <div class="masthead-content">
+      <div class="container">
+
+        <div class="card card-body text-dark p-sm-5 p-3">
+          <h1 class="h3 mt-3 mb-3">Oppss.. We've Encountered An Error</h1>
+          <p>Please <a class="text-danger font-weight-bold" href="<?= site_url('free-name-card-20190709'); ?>">try again</a> or contact our technical support <a class="text-danger font-weight-bold" href="https://api.whatsapp.com/send?phone=601156293646&text=<?= $text; ?>">+6018-871 3130</a> (WhatsApp)</p>
+
+          <?php if ( ! empty($error)) : ?>
+            <p>Error Code: <?= $error; ?></p>
+          <?php endif; ?>
+          <!--- https://www.flaticon.com/free-icon/warning_595067#term=error&page=1&position=32 --->
+          <img class="img-fluid mx-auto mt-5 mb-sm-3 mb-5" width="150rem" src="https://image.flaticon.com/icons/svg/595/595067.svg">
+        </div>
+
+      </div>
+    </div><!--- END Masthead Content --->
+    <div class="bg-circle-1 bg-circle"></div>
+    <div class="bg-circle-2 bg-circle"></div>
+    <div class="bg-circle-3 bg-circle"></div>
+    <div class="bg-circle-4 bg-circle"></div>
+  </header><!--- END Section Masthead --->
+</body>
+</html>
