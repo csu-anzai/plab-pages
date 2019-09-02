886032175758d832b8ae98a581f4266375863f70
Xavier Loo <xavier@printinglab.my>
2019-09-02 17:38:51 +0800

    Update environment config files

--- application/config/development/config.php
+++ application/config/development/config.php
@@ -1,30 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
-/*
-|--------------------------------------------------------------------------
-| Base Site URL
-|--------------------------------------------------------------------------
-|
-| URL to your CodeIgniter root. Typically this will be your base URL,
-| WITH a trailing slash:
-|
-|	http://example.com/
-|
-| WARNING: You MUST set this value!
-|
-| If it is not set, then CodeIgniter will try guess the protocol and path
-| your installation, but due to security concerns the hostname will be set
-| to $_SERVER['SERVER_ADDR'] if available, or localhost otherwise.
-| The auto-detection mechanism exists only for convenience during
-| development and MUST NOT be used in production!
-|
-| If you need to allow multiple domains, remember that this file is still
-| a PHP script and you can easily do that on your own.
-|
-*/
-$config['base_url'] = 'https://www.printinglab.my/pages/';
-
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -42,7 +17,7 @@ $config['index_page'] = '';
 | Static Folder URL
 |--------------------------------------------------------------------------
 */
-$config['asset_url'] = 'https://static.printinglab.my';
+$config['asset_url'] = 'http://localhost/edsa-static.printinglab.my/';
 
 /*
 |--------------------------------------------------------------------------
@@ -66,4 +41,4 @@ $config['asset_url'] = 'https://static.printinglab.my';
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 1;
+$config['log_threshold'] = 0;
