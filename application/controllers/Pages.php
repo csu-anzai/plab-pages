b702baf32c264ed766a4d01eb4161a773c828381
Xavier Loo <xavier@printinglab.my>
2019-09-02 17:22:33 +0800

    Update Pages.php
    Update pages loading mechanism

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -13,6 +13,7 @@ class Pages extends CI_Controller {
 		if (method_exists($this, $method)) {
 			return call_user_func_array(array($this, $method), $params);
 		} else {
+			/*
 			$folder = str_replace('_', '-', mb_strtolower($method));
 
 			if (empty($params[0])) {
@@ -20,9 +21,34 @@ class Pages extends CI_Controller {
 			} else {
 				$page = $params[0];
 			}
+			*/
 
-			$file = $folder.'/'.$page.'.php';
+			// If user visit pages via short url "p"
+			if ($method == 'p') {
+				empty($params[0]) && show_404();
+				$method = $params[0];
+				unset($params[0]);
+				sort($params);
+			}
+
+			$method = str_replace('_', '-', mb_strtolower($method));
+
+			// Load views file
+			$file = $method.'.php';
+			if (file_exists(VIEWPATH.$file)) {
+				$this->load->view($file);
+				return;
+			}
+
+			// Load views file from folder
+			if (empty($params[0])) {
+				$page = 'index';
+			} else {
+				$page = $params[0];
+			}
+			$file = $method.'/'.$page.'.php';
 			file_exists(VIEWPATH.$file) OR show_404();
+			
 			$this->load->view($file);
 		}
 	}
